67d7ade774aa485bb57595fff4b6f44b234d0a10
John Macaluso <John.Macaluso@infor.com>
2018-08-23 14:34:19 -0400

    cat filters

--- src/components/UsersPage.js
+++ src/components/UsersPage.js
@@ -4,22 +4,62 @@ import sampleUsers from '../sample-users';
 
 export default class UsersPage extends Component {
   state = {
-    sampleUsers
+    sampleUsers,
+    category: '',
+    sortAsc: false,
+    sortDesc: false
+  }
+
+  radioFilter = (e) => {
+    this.setState({
+      category: e.target.value
+    });
   }
 
   render() {
-    const { sampleUsers } = this.state;
+    const { sampleUsers, category } = this.state;
+    const featuredUsers = sampleUsers.data.map((user, i) => <User key={i} user={user} />);
+    let usersCategory;
+
+    if (category) {
+      usersCategory = sampleUsers.data
+        .filter(user => user.category === category)
+        .map((user, i) => <User key={i} user={user} />);
+    }
 
     return (
       <section className="users-page">
         <header>
           <h1>Users Grid</h1>
           <div className="filters">
-
+            <div className="filter-radio">
+              <label>
+                <input type="radio" name="category" value="cat1" onChange={(e) => this.radioFilter(e)}/>
+                Category 1
+              </label>
+            </div> 
+            <div className="filter-radio">
+              <label>
+                <input type="radio" name="category" value="cat2" onChange={(e) => this.radioFilter(e)} />
+                Category 2
+              </label>
+            </div> 
+            <div className="filter-radio">
+              <label>
+                <input type="radio" name="category" value="cat3" onChange={(e) => this.radioFilter(e)} />
+                Category 3
+              </label>
+            </div> 
+            <div className="filter-radio">
+              <label>
+                <input type="radio" name="category" value="cat4" onChange={(e) => this.radioFilter(e)} />
+                Category 4
+              </label>
+            </div> 
           </div>
         </header>
         <div className="users-grid">
-          {sampleUsers.data.map((user, i) => <User key={i} user={user} />)}
+          {category ? usersCategory : featuredUsers}
         </div>
       </section>
     )
