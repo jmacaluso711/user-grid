2f3c22a17bcea27c6a7f0f1979e9492d2c654983
John Macaluso <John.Macaluso@infor.com>
2018-08-23 17:54:02 -0400

    sort and filter

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const User = ({ user }) => (
-  <div className={"user " + user.category}>
+  <div className={"user user-priority--" + user.priority}>
     <h2 className="user-title">{user.name}</h2>
     <span className="user-age">Age: {user.age}</span>
     <span className="user-cat">Category: {user.category}</span>
--- src/components/UsersPage.js
+++ src/components/UsersPage.js
@@ -4,27 +4,55 @@ import sampleUsers from '../sample-users';
 
 export default class UsersPage extends Component {
   state = {
-    sampleUsers,
+    users: sampleUsers,
     category: '',
-    sortAsc: false,
-    sortDesc: false
   }
 
-  radioFilter = (e) => {
+  filterBy = (e) => {
     this.setState({
       category: e.target.value
     });
   }
 
+  sortBy = (e) => {
+    let users = Object.assign({}, this.state.users);
+    let sortedUsers;
+
+    switch(e.target.value) {
+      case 'asc':
+        sortedUsers = users.data.sort((a, b) => {
+          return a.name > b.name ? 1 : -1;
+        });
+        break;
+      case 'desc':
+        sortedUsers = users.data.sort((a, b) => {
+          return a.name > b.name ? -1 : 1;
+        });
+        break;
+      case 'priority':
+        sortedUsers = users.data.sort((a, b) => {
+          return a.priority > b.priority ? 1 : -1;
+        })
+        break;
+      case 'featured':
+        sortedUsers = users.data;
+        break;
+      default:
+        return null;
+    }
+
+    users.data = sortedUsers;
+    this.setState({users});
+  }
+
   render() {
-    const { sampleUsers, category } = this.state;
-    const featuredUsers = sampleUsers.data.map((user, i) => <User key={i} user={user} />);
-    let usersCategory;
+    const { category, users } = this.state;
+    const featuredUsers = users.data.map((user, i) => <User key={i} user={user} />);
+    let userList = featuredUsers;
 
     if (category) {
-      usersCategory = sampleUsers.data
-        .filter(user => user.category === category)
-        .map((user, i) => <User key={i} user={user} />);
+      const usersCategory = users.data.filter(user => user.category === category);
+      userList = usersCategory.map((user, i) => <User key={i} user={user} />);;
     }
 
     return (
@@ -34,32 +62,34 @@ export default class UsersPage extends Component {
           <div className="filters">
             <div className="filter-radio">
               <label>
-                <input type="radio" name="category" value="cat1" onChange={(e) => this.radioFilter(e)}/>
+                <input type="radio" name="category" value="cat1" onChange={(e) => this.filterBy(e)}/>
                 Category 1
               </label>
             </div> 
             <div className="filter-radio">
               <label>
-                <input type="radio" name="category" value="cat2" onChange={(e) => this.radioFilter(e)} />
+                <input type="radio" name="category" value="cat2" onChange={(e) => this.filterBy(e)} />
                 Category 2
               </label>
             </div> 
             <div className="filter-radio">
               <label>
-                <input type="radio" name="category" value="cat3" onChange={(e) => this.radioFilter(e)} />
+                <input type="radio" name="category" value="cat3" onChange={(e) => this.filterBy(e)} />
                 Category 3
               </label>
-            </div> 
-            <div className="filter-radio">
-              <label>
-                <input type="radio" name="category" value="cat4" onChange={(e) => this.radioFilter(e)} />
-                Category 4
-              </label>
-            </div> 
+            </div>
+          </div>
+          <div className="sorts">
+            <select onChange={(e) => this.sortBy(e)}> 
+              <option value="featured">Featured</option>
+              <option value="asc">Sort A-Z</option>
+              <option value="desc">Sort Z-A</option>
+              <option value="priority">Sort by Priority</option>
+            </select>
           </div>
         </header>
         <div className="users-grid">
-          {category ? usersCategory : featuredUsers}
+          {userList}
         </div>
       </section>
     )
