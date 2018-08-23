b1b187317d64eb296ce4abd3c641ed0a79baaee1
John Macaluso <John.Macaluso@infor.com>
2018-08-23 11:26:43 -0400

    login styles

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ const User = ({ user }) => (
 )
 
 class UsersPage extends Component {
-  
   state = {
     sampleUsers
   }
@@ -63,7 +62,6 @@ class UsersPage extends Component {
 }
 
 class Login extends Component {
-
   state = {
     redirectToUsers: false,
     emailError: '',
@@ -90,7 +88,7 @@ class Login extends Component {
 
     if (!formData.password.match(passwordRegex)) {
       isError = true;
-      errors.passwordError = 'Password must be at leaset 10 characters and contain 1 special character.'
+      errors.passwordError = 'Password must be at least 10 characters and contain 1 special character.'
     } else {
       errors.passwordError = '';
     }
@@ -112,7 +110,8 @@ class Login extends Component {
     return isError;
   }
 
-  login = () => {
+  login = (e) => {
+    e.preventDefault();
     let formData = {
       email: this.email.value,
       password: this.password.value,
@@ -136,11 +135,6 @@ class Login extends Component {
     }
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.login();
-  }
-
   render() {
 
     const { redirectToUsers, emailError, passwordError, accessError } = this.state;
@@ -150,30 +144,32 @@ class Login extends Component {
     }
 
     return (
-      <div>
-        <h1>Login</h1>
-        {accessError ? <span className="form-error">{accessError}</span> : null}
-        <form onSubmit={(e) => this.handleSubmit(e)} noValidate>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              ref={(input) => this.email = input}
-              type="email"
-              name="email"
-            />
-            {emailError ? <span className="form-error">{emailError}</span> : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              ref={(input) => this.password = input}
-              type="password"
-              name="password"
-            />
-            {passwordError ? <span className="form-error">{passwordError}</span> : null}
-          </div>
-          <button>Login</button>
-        </form>
+      <div className="login-page">
+        <div className="login-container">
+          <h1>Login</h1>
+          {accessError ? <span className="form-error form-error--access">{accessError}</span> : null}
+          <form onSubmit={(e) => this.login(e)} noValidate>
+            <div className={emailError ? "form-group form-group--error" : "form-group"}>
+              <label htmlFor="email">Email</label>
+              <input
+                ref={(input) => this.email = input}
+                type="email"
+                name="email"
+              />
+              {emailError ? <span className="form-error">{emailError}</span> : null}
+            </div>
+            <div className={passwordError ? "form-group form-group--error" : "form-group"}>
+              <label htmlFor="password">Password</label>
+              <input
+                ref={(input) => this.password = input}
+                type="password"
+                name="password"
+              />
+              {passwordError ? <span className="form-error">{passwordError}</span> : null}
+            </div>
+            <button>Login</button>
+          </form>
+        </div>
       </div>
     )
   }
